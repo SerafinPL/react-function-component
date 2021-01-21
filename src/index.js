5fdc5b8eb82b8768c494e3956578d486183ea6ec
serafinpl <ser.burberibar@gmail.com>
2021-01-21 20:46:02 +0100

    pierwszy moduł Literek na Comp. Funkcyjnych

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Link from './Linki/linki';
 
-const test =() => {
+import Link from './Linki/linki';
+import Napis from './mods/Literki/napis';
+
+
+
+
+
+
+const literki =() => {
 
 ReactDOM.render(
-  <div>
-    <App />
-  </div>,
+  <Napis/>,
   document.getElementById('root')
 );
 }
 
 ReactDOM.render(
   <div>
-    <Link children="próba1" click={test}/>
+    <Link children="Literki" click={literki}/>
     <Link children="próba2" />
     <Link children="próba3" />
   </div>,
