66da2df5620af48e5de32dc47c986fc5643e2f8d
serafinpl <ser.burberibar@gmail.com>
2021-01-28 16:07:13 +0100

    Kwadrat zrobiony

--- src/mods/Karty/Karty.js
+++ src/mods/Karty/Karty.js
@@ -1,2 +1,4 @@
 import React from 'react';
 
+import Kwadrat from './Kwadrat';
+import Etykieta from './Etykieta';
---
+++ src/mods/Karty/Kwadrat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const stylKwadratu = {
+	height: 150,
+    backgroundColor: this.props.color
+}
+
+const Kwadrat = (props) => {
+	<div style = {stylKwadratu}>
+    </div>
+}
+
+export default Kwadrat;
