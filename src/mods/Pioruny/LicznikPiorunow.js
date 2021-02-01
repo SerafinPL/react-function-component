535ae6ced049d2c53c14a66c6960fa8145977cb6
serafinpl <ser.burberibar@gmail.com>
2021-02-01 15:49:37 +0100

    pioruny UseState function

--- src/mods/Pioruny/LicznikPiorunow.js
+++ src/mods/Pioruny/LicznikPiorunow.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 import styles from './LicznikPiorunow.module.css';
 
 const Licznik = () => {
 
+	
+
+	const [piorunyState, piorunySetState ] = useState(
+			0
+		);
+
+	const timerStorm = () =>{
+
+		let pior = piorunyState;
+		piorunySetState(
+			pior + 100
+		)
+
+		
+	}
+
+	setInterval(timerStorm, 1000);
+
 	return(
-		<h1 className={styles.licznik}>Licznik</h1>
+		<h1 className={styles.licznik}>{piorunyState}</h1>
 	)
 
 }
